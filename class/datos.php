424142f5c28364f1ebc11f3f8441a08eea36dfa5
Santiago Sibilia <santiagosibilia1998@hotmail.com>
2024-07-02 16:54:51 -0300

    vamo a ver si funca

--- class/datos.php
+++ class/datos.php
@@ -1,112 +1,8 @@
-<?php 
+<?php
+        include_once "../tablas.php";
+
     class datos extends DAO{
-       /* private $IdSala;
-        private $CiEmpleado;
-        private $fechaReserva;
-        private $horaInicio;
-        private $horaFin;
-        private $motivo;
-        //fecha y hora de la creacion Ees automatica 
-        private $estado; //donde el estado de la reserva puede ser activa o finalizada 
-        //CONSTRUCTOR ----------------------------------------------------------------
-        public function __construct($IdSala,$CiEmpleado,$fechaReserva,$horaInicio,$horaFin,$motivo,$estado){
-            $this->IdSala=$IdSala;
-            $this->CiEmpleado=$CiEmpleado;
-            $this->fechaReserva=$fechaReserva;
-            $this->horaInicio=$horaInicio;
-            $this->horaFin=$horaFin;
-            $this->motivo=$motivo;
-            $this->estado=$estado;
-        }
-        //GETTERS ----------------------------------------------------------------
-        public function getIdSala(){
-            return $this->IdSala;
-        }
-        public function getCiEmpleado(){
-            return $this->CiEmpleado;
-        }
-        public function getFechaReserva(){
-            return $this->fechaReserva;
-        }
-        public function getHoraInicio(){
-            return $this->horaInicio;
-        }
-        public function getHoraFin(){
-            return $this->horaFin;
-        }
-        public function getMotivo(){
-            return $this->motivo;
-        }
-        public function getEstado(){
-            return $this->estado;
-        }
-        //SETTERS ----------------------------------------------------------------
-        public function setIdSala($IdSala){
-            $this->IdSala=$IdSala;
-        }
-        public function setCiEmpleado($CiEmpleado){
-            $this->CiEmpleado=$CiEmpleado;
-        }
-        public function setFechaReserva($fechaReserva){
-            $this->fechaReserva=$fechaReserva;
-        }
-        public function setHoraInicio($horaInicio){
-            $this->horaInicio=$horaInicio;
-        }
-        public function setHoraFin($horaFin){
-            $this->horaFin=$horaFin;
-        }
-        public function setMotivo($motivo){
-            $this->motivo=$motivo;
-        }
-        public function setEstado($estado){
-            $this->estado=$estado;
-        }
-        //DESTRUCTOR ----------------------------------------------------------------
-        public function __destruct(){
-            $this->IdSala=null;
-            $this->CiEmpleado=null;
-            $this->fechaReserva=null;
-            $this->horaInicio=null;
-            $this->horaFin=null;
-            $this->motivo=null;
-            $this->estado=null;
-        }
-        //TOSTRING ----------------------------------------------------------------
-        public function __toString(){
-            return $this->IdSala." ".$this->CiEmpleado." ".$this->fechaReserva." ".$this->horaInicio." ".$this->horaFin." ".$this->motivo." ".$this->estado;
-        }
-        //METODOS ----------------------------------------------------------------
-        public function mostrarDatos(){
-            echo $this->IdSala." ".$this->CiEmpleado." ".$this->fechaReserva." ".$this->horaInicio." ".$this->horaFin." ".$this->motivo." ".$this->estado;
-        }*/
-        /*public function modificarDatos($IdSala,$CiEmpleado,$fechaReserva,$horaInicio,$horaFin,$motivo,$estado){
-            $this->IdSala=$IdSala;
-            $this->CiEmpleado=$CiEmpleado;
-            $this->fechaReserva=$fechaReserva;
-            $this->horaInicio=$horaInicio;
-            $this->horaFin=$horaFin;
-            $this->motivo=$motivo;
-            $this->estado=$estado;
-        }*/
-        /*public function agregarDatos($IdSala,$CiEmpleado,$fechaReserva,$horaInicio,$horaFin,$motivo,$estado){
-            $this->IdSala=$IdSala;
-            $this->CiEmpleado=$CiEmpleado;
-            $this->fechaReserva=$fechaReserva;
-            $this->horaInicio=$horaInicio;
-            $this->horaFin=$horaFin;
-            $this->motivo=$motivo;
-            $this->estado=$estado;
-        }*/
-        /*public function buscarDatos($IdSala,$CiEmpleado,$fechaReserva,$horaInicio,$horaFin,$motivo,$estado){
-            $this->IdSala=$IdSala;
-            $this->CiEmpleado=$CiEmpleado;
-            $this->fechaReserva=$fechaReserva;
-            $this->horaInicio=$horaInicio;
-            $this->horaFin=$horaFin;
-            $this->motivo=$motivo;
-            $this->estado=$estado;
-        }*/
+    
 
         public function getDatos($ciEmpleado){
             $consulta = "SELECT * FROM datos AS d WHERE d.ciEmpleado = :ciEmpleado";
@@ -116,6 +12,28 @@
             }else return false;
         }
 
+        public function buscarDatos ($ciEmpleado) {
+            $consulta = $this->con->query("SELECT * FROM datos WHERE ciEmpleado = '$ciEmpleado'" );
+            $cantidadFilas = $consulta->rowCount();
+            if ($cantidadFilas > 0){
+                echo "<p align = center>El usuario tiene $cantidadFilas datos: </p>";
+                crearTabla();
+                crearCabezal("id sala", "ci empleado", "hora de inicio", "hora de fin");
+                while ($row = $consulta->fetch(PDO::FETCH_OBJ)){ //fecth(PDO::FETCH_OBJ /*$consulta->fetchAll(PDO::FETCH_ASSOC)*/)){
+                    $idSala = $row->idSala;
+                    $ciEmpleado = $row->ciEmpleado;
+                    $horaInicio = $row->horaInicio;
+                    $horaFin = $row->horaFin;
+
+                    $dato = new datos($idSala, $ciEmpleado, $horaInicio, $horaFin);
+                    ingresarDatosConFila($idSala, $ciEmpleado, $horaInicio, $horaFin);
+                }
+                cerrarTabla();
+
+                //return $datos;
+            } else //return false;
+            echo "<p align = center>El empleado no tiene datos. </p>";
+        }
 
     }
 ?>
