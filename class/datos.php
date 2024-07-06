8635290f7c7d99b44d9387eee0b59b96e82758ef
Santiago Sibilia <santiagosibilia1998@hotmail.com>
2024-07-06 11:51:37 -0300

    ADNADA EL CIERRE AUTOMATICO CDE SESION Y MODIFIQUE UN CACHO LA PAG PRINCIPAL

--- class/datos.php
+++ class/datos.php
@@ -70,40 +70,62 @@
             $cantidadFilas = $consulta->rowCount();
             if ($cantidadFilas > 0){
                 echo "<p align = center>El usuario tiene $cantidadFilas datos: </p>";
-                echo "<table width='60%' align='center' border='1'>";
+                /*echo "<table width='60%' align='center' border='1'>";
                 echo "<th bgcolor='gray'>", "Imagen de la Sala", "</th>";
                 echo "<th bgcolor='gray'>", "Nombre de la Sala", "</th>";
                 echo "<th bgcolor='gray'>", "hora de inicio", "</th>";
                 echo "<th bgcolor='gray'>", "hora de fin", "</th>";
                 echo "<th bgcolor='gray'>", "Nombre del Empleado", "</th>";
                 echo "<th bgcolor='gray'>", "Apellido del Empleado", "</th>";
-                echo "<th bgcolor='gray'>", "ci empleado", "</th>";
-                while ($row = $consulta->fetch(PDO::FETCH_OBJ)){
-                    echo "<tr>";
-                        $idSala = $row->IdSala;
-                        $ciEmpleado = $row->CiEmpleado;
-                        $horaInicio = $row->horaInicio;
-                        $horaFin = $row->horaFin;
-                        $img = $row->foto;
-                        $name = $row->primerNombre;
-                        $apellido = $row->primerApellido;
-                        $nombre = $row->nombre;
-                        //echo "<td> <div class='cell'><a href='detalleReserva.php?id=$idSala'><img src='img/$img' width='100' height='100'></a></div></td>";
-                        if (empty($img)){
-                            echo "<td> <div class='cell'><a href='detalleReserva.php?id=$idSala'><img src='uploads/default-pp.png' width='50%' height='50%'></a></div></td>";
-                        }else{
-                            echo "<td> <div class='cell'><a href='detalleReserva.php?id=$idSala'><img src='uploads/$img' width='50%' height='50%'></a></div></td>";
+                echo "<th bgcolor='gray'>", "ci empleado", "</th>";*/
+                echo"<main class='table' id='customers_table'>";
+                echo"<section class='table__header'>";
+                    echo"<h1>Tabla de Salas</h1>";
+                echo"</section>";
+                echo "<section class='table__body'>";
+                echo "<table>";
+                    echo "<thead>";
+                        echo "<tr>";
+                            echo "<th>" . "Imagen de la Sala" . "</th>";
+                            echo "<th>", "Nombre de la Sala", "</th>";
+                            echo "<th>", "hora de inicio", "</th>";
+                            echo "<th>", "hora de fin", "</th>";
+                            echo "<th>", "Nombre del Empleado", "</th>";
+                            echo "<th>", "Apellido del Empleado", "</th>";
+                            echo "<th>", "ci empleado", "</th>";
+                        echo "</tr>";
+                    echo "</thead>";
+                    echo "<tbody>";
+                        while ($row = $consulta->fetch(PDO::FETCH_OBJ)){
+                            echo "<tr>";
+                                $idSala = $row->IdSala;
+                                $ciEmpleado = $row->CiEmpleado;
+                                $horaInicio = $row->horaInicio;
+                                $horaFin = $row->horaFin;
+                                $img = $row->foto;
+                                $name = $row->primerNombre;
+                                $apellido = $row->primerApellido;
+                                $nombre = $row->nombre;
+                                //echo "<td> <div class='cell'><a href='detalleReserva.php?id=$idSala'><img src='img/$img' width='100' height='100'></a></div></td>";
+                                if (empty($img)){
+                                    echo "<td> <div class='cell'><a href='detalleReserva.php?id=$idSala'><img src='uploads/default-pp.png' width='50%' height='50%'></a></div></td>";
+                                }else{
+                                    echo "<td> <div class='cell'><a href='detalleReserva.php?id=$idSala'><img src='uploads/$img' width='50%' height='50%'></a></div></td>";
+                                }
+                                echo "<td>".$nombre."</td>";
+                                echo "<td>".$horaInicio."</td>";
+                                echo "<td>".$horaFin."</td>";
+                                echo "<td>".$name."</td>";
+                                echo "<td>".$apellido."</td>";
+                                echo "<td>".$ciEmpleado."</td>";
+                                                    
+                            echo "</tr>";
                         }
-                        echo "<td>".$nombre."</td>";
-                        echo "<td>".$horaInicio."</td>";
-                        echo "<td>".$horaFin."</td>";
-                        echo "<td>".$name."</td>";
-                        echo "<td>".$apellido."</td>";
-                        echo "<td>".$ciEmpleado."</td>";
-                                            
-                    echo "</tr>";
-                }
-                echo "</table>";
+                    echo "</tbody>";
+                    echo "</table>";
+                echo "</section>";
+                echo "</main>";
+               
             } else
                 echo "<p align = center>No existen salas las cuales su hora de finalizacion sea mayor a la hora actual. </p>";
         }
--- index.php
+++ index.php
@@ -7,16 +7,23 @@ if (isset($_SESSION['usuario'])) {
 ?>
 <!DOCTYPE html>
 <html>
-<head>
+<head><script src="../assets/js/color-modes.js"></script>
     <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="refresh" content="600; url=login.php?error=La session expiro">
 	<title>Home</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <!--<link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+
     <link rel="stylesheet" type="text/css" href="css/style-home.css">
+
+    
 </head>
 <body>
-    <nav>
+   <nav>
         <ul class="sidebar">
             <li onclick=hideSidebar()><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26" fill="#000000"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
             <li><a href="index.php">Inicio</a></li>
@@ -42,6 +49,52 @@ if (isset($_SESSION['usuario'])) {
             <li onclick=showSidebar()><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26" fill="#000000"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg> </a>
         </ul>
     </nav>
+    <!--<header class="p-3 text-bg-dark">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="index.php" class="nav-link px-2 text-secondary">Inicio</a></li>
+          <?php if($_SESSION['usuario']['tipoUsuario'] == 'administrador'){?>
+            <li><a href="#" class="nav-link px-2 text-white">Asignacion de reservas</a></li>
+            <li><a href="ingresar_sala.php" class="nav-link px-2 text-white">Ingreso de salas</a></li>
+            <?php } ?>
+          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+        </ul>
+        <ul class="nav col-30 col-lg-auto me-lg-auto mb-6 justify-content-center mb-md-0">
+            <li> Hola, <?php echo $_SESSION['usuario']['primerNombre']; ?></li>
+            <li>  <?php $_SESSION['usuario']['primerApellido']; ?></li>
+        </ul>
+        <ul class="nav col-30 col-lg-auto me-lg-auto mb-6 justify-content-center mb-md-0">
+            <li onclick=showSidebar()>
+            <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"> 
+                <?php if (empty($_SESSION['usuario']['fotoPerfil'])){?>
+                        <img src="uploads/default-pp.png" width="32" height="32"  class="img-fluid rounded-circle">
+                    <?php ;}else{?>
+                        <img src="uploads/<?=$_SESSION['usuario']['fotoPerfil']?>" width="32" height="32"  class="img-fluid rounded-circle">
+                    <?php ;}?>
+            </a>
+            </li>
+        </ul>
+        <ul class="sidebar nav col-30 col-lg-auto me-lg-auto mb-6 mb-md-0">
+            <li onclick=hideSidebar()>
+                <a href="#"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26" fill="#000000">
+                        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+                    </svg>
+                </a>
+            </li>
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+        </ul>
+      </div>
+  </header>-->
+  
     <?php
                 /*if (isset($_COOKIE['errorLogin']))
                     echo "<p>" . $_COOKIE['errorLogin'] . "</p>";*/
@@ -51,7 +104,7 @@ if (isset($_SESSION['usuario'])) {
                     <p class="exito"><?php echo $_GET['exito']?></p> 
             <?php }?>
     <a href="logout.php">Logout</a>
-    <h2>Tabla de Salas</h2>
+    
     <!--<table>
         <thead>
             <tr>
@@ -114,6 +167,9 @@ if (isset($_SESSION['usuario'])) {
             sidebar.style.display = 'none'
         }
     </script>
+
+<!--<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>-->
 
     
 </body>
