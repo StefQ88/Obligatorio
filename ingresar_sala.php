9ea03b028405a8c7ce0bb87c2b5df688f9ff002c
Santiago Sibilia <santiagosibilia1998@hotmail.com>
2024-07-08 22:26:55 -0300

    lo arregle, LO ARREGLE FALTA LA NAV EN INGRESAR SALA

--- ingresar_sala.php
+++ ingresar_sala.php
@@ -9,53 +9,6 @@
 </head>
 
 <body>
-<nav>
-        <ul class="sidebar">
-            <li onclick=hideSidebar()><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26" fill="#000000">
-                        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-                    </svg></a></li>
-            <div class="user-info">
-                <?php if ($_SESSION['usuario']['tipoUsuario'] == 'empleado') { ?>
-                    <li><a><?php echo $_SESSION['usuario']['primerNombre'] ?></a></li>
-                    <li><a><?php echo $_SESSION['usuario']['primerApellido'] ?></a></li>
-                    <?php if (empty($_SESSION['usuario']['fotoPerfil'])) { ?>
-                        <li> <a><img src="uploads/default-pp.png" class="img-fluid rounded-circle" style="width: 6%;"></a> </li>
-                    <?php } else { ?>
-                        <li> <img src="uploads/<?= $_SESSION['usuario']['fotoPerfil'] ?>" class="img-fluid rounded-circle"> </li>
-                    <?php } ?>
-                <?php } else if ($_SESSION['usuario']['tipoUsuario'] == 'administrador') { ?>
-                    <li><a href="test.php">OfficeSpaces</a></li>
-            </div>
-        <?php } ?>
-        <li><a href="index.php">Inicio</a></li>
-        <?php if ($_SESSION['usuario']['tipoUsuario'] == 'administrador') { ?>
-            <li><a href="asignar_reservas.php">Asignacion de reservas</a></li>
-            <li><a href="ingresar_sala.php">Ingreso de salas</a></li>
-        <?php } ?>
-        <li><a href="historial_reservas.php">Historial de reservas</a></li>
-        <li><a href="perfil.php">Perfil</a></li>
-        <li><a href="logout.php">Salir</a></li>
-        </ul>
-        <ul>
-            <li><a href="index.php">OfficeSpaces</a></li>
-            <?php if (empty($_SESSION['usuario']['fotoPerfil'])) { ?>
-                <li> <a><img src="uploads/default-pp.png" class="img-fluid rounded-circle" style="width: 6%;"></a> </li>
-            <?php } else { ?>
-                <li> <img src="uploads/<?= $_SESSION['usuario']['fotoPerfil'] ?>" class="img-fluid rounded-circle"> </li>
-            <?php } ?>
-            <li onclick=showSidebar()><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26" fill="#000000">
-                        <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-                    </svg> </a>
-        </ul>
-    </nav>
-  
-    <?php
-                if (isset($_GET['error'])){ ?>
-                    <p class="error"><?php echo $_GET['error']; }?></p>
-                <?php if (isset($_GET['exito'])){ ?>
-                    <p class="exito"><?php echo $_GET['exito']?></p> 
-            <?php }?>
-    
     <div class="container">
         <div class="title">Ingreso de nueva sala</div>
         <div class="content">
@@ -100,17 +53,6 @@
             </form>
         </div>
     </div>
-    <script>
-        function showSidebar() {
-            const sidebar = document.querySelector('.sidebar');
-            sidebar.style.display = 'flex'
-        }
-
-        function hideSidebar() {
-            const sidebar = document.querySelector('.sidebar');
-            sidebar.style.display = 'none'
-        }
-    </script>
 </body>
 
 </html>
