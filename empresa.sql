1f22faac032d24c4ad0c8791fc3e5e2a577be8a1
Santiago Sibilia <santiagosibilia1998@hotmail.com>
2024-07-09 17:48:35 -0300

    BD2

--- empresa.sql
+++ empresa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 09-07-2024 a las 20:42:00
+-- Tiempo de generación: 09-07-2024 a las 20:48:04
 -- Versión del servidor: 8.2.0
 -- Versión de PHP: 8.2.13
 
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `saladeconferencias` (
   `estado` enum('disponible','no_disponible') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `foto` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
