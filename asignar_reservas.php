c37a8b7222c56d3d1b8c952e4196c4cd72e905ae
Santiago Sibilia <santiagosibilia1998@hotmail.com>
2024-07-09 17:25:01 -0300

    anda el la reserva no se identifica con ningun genero

--- asignar_reservas.php
+++ asignar_reservas.php
@@ -80,25 +80,25 @@
         <form action="procesar_reserva.php" method="post">
             <div class="mb-3">
                 <label for="sala" class="form-label">Sala</label>
-                <select name="sala" class="form-select" required>
+                <select name="sala" class="form-select">
                     <?php $sala->listarSalas(); ?>
                 </select>
             </div>
             <div class="mb-3">
                 <label for="fechaReserva" class="form-label">Fecha de la reserva</label>
-                <input type="date" name="fechaReserva" class="form-control" required>
+                <input type="date" name="fechaReserva" class="form-control">
             </div>
             <div class="mb-3">
                 <label for="horaInicio" class="form-label">Hora de Inicio</label>
-                <input type="time" name="horaInicio" class="form-control" required>
+                <input type="time" name="horaInicio" class="form-control">
             </div>
             <div class="mb-3">
                 <label for="horaFin" class="form-label">Hora de Fin</label>
-                <input type="time" name="horaFin" class="form-control" required>
+                <input type="time" name="horaFin" class="form-control">
             </div>
             <div class="mb-3">
                 <label for="usuario" class="form-label">Usuario</label>
-                <select name="usuario" class="form-select" required>
+                <select name="usuario" class="form-select">
                     <?php $usuario->listarUsuarios(); ?>
                 </select>
             </div>
--- procesar_reserva.php
+++ procesar_reserva.php
@@ -8,15 +8,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      $fechaInicio = $_POST['horaInicio'];
      $fechaFin = $_POST['horaFin'];
 
+    if (empty($salaId)){
+        header("Location: asignar_reservas.php?error=la sala es requerido");
+         exit;
+    }
+    else if (empty($usuarioId)){
+        header("Location: asignar_reservas.php?error=el usuario es requerido");
+         exit;
+    
+    }
+    else if (empty($fechaReserva)){
+        header("Location: asignar_reservas.php?error=la fecha de la reserva es requerido");
+         exit;
 
-     //if ($dato->validarFechas($fechaInicio, $fechaFin)) {
+    }else if (empty($fechaInicio)){
+        header("Location: asignar_reservas.php?error=la hora de inicio es requerido");
+         exit;
+    }
+    else if (empty($fechaFin)){
+        header("Location: asignar_reservas.php?error=la hora de fin es requerido");
+         exit;
+    }
+     else{
+        if ($dato->validarFechas($fechaInicio, $fechaFin)) {
      // if ($dato->verificarDisponibilidad($salaId, $fechaReserva,$fechaReserva,$fechaInicio, $fechaFin)) {
      $dato->crearReserva($salaId, $usuarioId, $fechaReserva, $fechaInicio, $fechaFin);
      header('Location: index.php?exito=Reserva realizada con éxito');
      /* } else {
             header('Location: asignar_reservas.php?error=La sala ya está reservada en la fecha seleccionada');
         }*/
-     /* } else {
-        header('Location: asignar_reservas.php?error=Las fechas son inválidas');
-    }*/
+     } else {
+        header('Location: asignar_reservas.php?error=Las horas o la fecha son inválidas');
+    }
+     }   
+     
 }
